6f34710a079406723b5f6975bd9ffdf2b318b361
Mouhcine Mendil <mouhcine.mendil@irt-saintexupery.com>
2022-05-20 16:58:54 +0200

    Add CI of High level API + Update extra requirements of setup.py

---
+++ tests/test_hi_level.py
@@ -0,0 +1,104 @@
+from random import random
+import pytest
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from puncc.utils import average_coverage
+from puncc.common.confomalizers import (
+    SplitCP,
+    LocallyAdaptiveCP,
+    CQR,
+    CvPlus,
+    EnbPI,
+    AdaptiveEnbPI,
+)
+
+
+@pytest.mark.parametrize(
+    "alpha, random_state",
+    [(0.1, 42)],
+)
+def test_split_cp(diabetes_data, alpha, random_state):
+    # Get data
+    (X_train, X_test, y_train, y_test) = diabetes_data
+    # split train data into fit and calibration
+    X_fit, X_calib, y_fit, y_calib = train_test_split(
+        X_train, y_train, random_state=random_state
+    )
+    # Create linear regression object
+    regr_model = linear_model.LinearRegression()
+    # CP method initialization
+    split_cp = SplitCP(regr_model)
+    # The fit method trains the model and computes the residuals on the
+    # calibration set
+    split_cp.fit(X_fit, y_fit, X_calib, y_calib)
+    # The predict method infers prediction intervals with respect to
+    # the risk alpha
+    y_pred, y_pred_lower, y_pred_upper = split_cp.predict(X_test, alpha=alpha)
+    # Compute marginal coverage
+    coverage = average_coverage(y_test, y_pred_lower, y_pred_upper)
+    assert (y_pred is not None) and (coverage >= 1 - alpha)
+
+
+@pytest.mark.parametrize(
+    "alpha, random_state",
+    [(0.1, 42)],
+)
+def test_locally_adaptive_cp(diabetes_data, alpha, random_state):
+    # Get data
+    (X_train, X_test, y_train, y_test) = diabetes_data
+    # split train data into fit and calibration
+    X_fit, X_calib, y_fit, y_calib = train_test_split(
+        X_train, y_train, random_state=random_state
+    )
+    # Create linear regression object
+    regr_model = linear_model.LinearRegression()
+    # Create RF regression object
+    var_model = RandomForestRegressor(
+        n_estimators=100, random_state=random_state
+    )
+    # CP method initialization
+    la_cp = LocallyAdaptiveCP(regr_model, var_model)
+    la_cp.fit(X_fit, y_fit, X_calib, y_calib)
+    y_pred, y_pred_lower, y_pred_upper, var_pred = la_cp.predict(
+        X_test, alpha=alpha
+    )
+    # Compute marginal coverage
+    coverage = average_coverage(y_test, y_pred_lower, y_pred_upper)
+    assert (
+        (y_pred is not None)
+        and (var_pred is not None)
+        and (coverage >= 1 - alpha)
+    )
+
+
+@pytest.mark.parametrize(
+    "alpha, random_state",
+    [(0.1, 42)],
+)
+def test_cqr(diabetes_data, alpha, random_state):
+    assert True
+
+
+@pytest.mark.parametrize(
+    "alpha, random_state",
+    [(0.1, 42)],
+)
+def test_cv_plus(diabetes_data, alpha, random_state):
+    assert True
+
+
+@pytest.mark.parametrize(
+    "alpha, random_state",
+    [(0.1, 42)],
+)
+def test_enbpi(diabetes_data, alpha, random_state):
+    assert True
+
+
+@pytest.mark.parametrize(
+    "alpha, random_state",
+    [(0.1, 42)],
+)
+def test_adaptive_enbpi(diabetes_data, alpha, random_state):
+    assert True
