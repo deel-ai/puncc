6fe10dbdcdab9529c60562667f41fde1a7efaab6
Mouhcine Mendil <mouhcine.mendil@irt-saintexupery.com>
2022-05-23 12:13:59 +0000

    Resolve "CI and Testing"

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn import datasets
+import pytest
+
+
+@pytest.fixture
+def diabetes_data():
+
+    # Load the diabetes dataset
+    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+    # Use only one feature
+    diabetes_X = diabetes_X[:, np.newaxis, 2]
+
+    # Split the data into training/testing sets
+    X_train = diabetes_X[:-100]
+    X_test = diabetes_X[-100:]
+
+    # Split the targets into training/testing sets
+    y_train = diabetes_y[:-100]
+    y_test = diabetes_y[-100:]
+
+    return X_train, X_test, y_train, y_test
